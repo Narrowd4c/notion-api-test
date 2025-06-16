78ea8484ffa44ded151fee51c8e91e7f94493e2b
john <aarontu803@gmail.com>
2025-06-17 02:04:35 +0800

    feat: 將功能套用至 Chrome extension

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
-import 'dotenv/config'
 
-import { Client } from '@notionhq/client';
+import { Client } from '@notionhq/client/';
 
-const notion = new Client({ auth:  process.env.NOTION_API_KEY });
+if (typeof window !== 'undefined') {
+  window.fetch = window.fetch.bind(window);
+}
 
-(async () => {
-  const blockId = process.env.NOTION_PAGE_ID;  // also accept page ID
+const notion = new Client({ auth:  import.meta.env.VITE_NOTION_API_KEY });
+
+async function addToPage(text) {
+  const blockId = import.meta.env.VITE_NOTION_PAGE_ID; // // also accept page ID
   const response = await notion.blocks.children.append({
     block_id: blockId,
     children: [
-      {
-        "heading_2": {
-          "rich_text": [
-            {
-              "text": {
-                "content": "Lacinato kale"
-              }
-            }
-          ]
-        }
-      },
       {
         "paragraph": {
           "rich_text": [
             {
               "text": {
-                "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
-                "link": {
-                  "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
-                }
+                "content": text,
               }
             }
           ]
@@ -37,4 +26,27 @@ const notion = new Client({ auth:  process.env.NOTION_API_KEY });
     ],
   });
   console.log(response);
-})();
+}
+
+const submitButton = document.getElementById('submitButton');
+const statusMessage = document.getElementById('statusMessage');
+
+submitButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const inputText = document.getElementById('inputField');
+  if (inputText.value) {
+    try {
+      await addToPage(inputText.value);
+      console.log('Text added to Notion page successfully');
+      statusMessage.textContent = '儲存成功！';
+      inputText.value = ''; // Clear the input field after submission
+    } catch (error) {
+
+      console.error('Error adding text to Notion page:', error);
+      statusMessage.textContent = '儲存失敗！';
+    }
+  } else {
+    console.warn('Input text is empty');
+    statusMessage.textContent = '請輸入內容！';
+  }
+});
